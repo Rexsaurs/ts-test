35a4b8369985013a46851a3b4feabae7e0a1d81a
daffisclr <daffisclr@gmail.com>
2024-08-11 18:22:26 +0700

    add livewire powergrid

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
 require('./bootstrap');
+
+import './../../vendor/power-components/livewire-powergrid/dist/powergrid';
+import './../../vendor/power-components/livewire-powergrid/dist/bootstrap5.css';
